8ee858362cf1f247cab5687c8222a8969676acaf
Prajwol Shrestha <shresthaprajwol161@gmail.com>
2025-03-15 12:20:26 +0545

    wip: abstract and sealed class

--- structure.cs
+++ structure.cs
@@ -1,26 +1,26 @@
-using System;
+// using System;
 
-namespace structure{
-    struct Book{
-        public string name;
-        public string author;
+// namespace structure{
+//     struct Book{
+//         public string name;
+//         public string author;
         
-        public string releaseDate;
+//         public string releaseDate;
 
-    }
-    enum Days{
-        sunday=12, monday, tuesday, wednesday, thrusday, friday, saturday
-    }
-    class Program {
-        static void Main(string[] args){
-            Console.WriteLine("Learning Structure");
-            Book book1;
-            book1.author="ram";
-            book1.name="Bhagwat Gita";
-            book1.releaseDate="before 5000 years";
+//     }
+//     enum Days{
+//         sunday=12, monday, tuesday, wednesday, thrusday, friday, saturday
+//     }
+//     class Program {
+//         static void Main(string[] args){
+//             Console.WriteLine("Learning Structure");
+//             Book book1;
+//             book1.author="ram";
+//             book1.name="Bhagwat Gita";
+//             book1.releaseDate="before 5000 years";
 
-            Console.WriteLine(book1.author);
-            Console.WriteLine((int)Days.sunday);
-        }
-    }
-}
+//             Console.WriteLine(book1.author);
+//             Console.WriteLine((int)Days.sunday);
+//         }
+//     }
+// }
