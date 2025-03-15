be2f752c51e71f070c6fca5d8b2b96050403d4b4
Prajwol Shrestha <shresthaprajwol161@gmail.com>
2025-03-15 13:43:10 +0545

    wip: generics, linq and collection in c#

--- Generics.cs
+++ Generics.cs
@@ -5,19 +5,24 @@ using System.Globalization;
 
 
 namespace genericsExample{
-
     class Program{
-        class Love<T>{
-            public string loverName=string.Empty;
-            public int relationship;
+        class Love<T,U>{
+            public required T name;
+            public required U age;
 
-            void LoverLove(){
+            public required T lover;
 
+            public void LovePlace<F>(F value){
+                    Console.WriteLine("love place is Sambriddi college and ture love is {0}",value);
             }
 
         }
         static void Main(string [] args){
-            
+            Love<string, int> mylove=new Love<string, int>(){name="parajwol",age=12,lover="parjwol"};
+            mylove.LovePlace<bool>(true);
+            // mylove.name="bhagwan";
+            // mylove.age=22;
+            // mylove.lover="prajwol";
         }
     }
 }
--- Linq.cs
+++ Linq.cs
@@ -1,4 +1,16 @@
-using System;
-using System.Linq;
+// using System;
+// using System.Linq;
 
+// namespace linqexample{
+//     class Program{
+//     static void Main(string [] args){
+//         int [] scores=new int [] {234,45,56,4565,23,6};
+//        IEnumerable<int> scoreQuery=from score in scores where score >80 select score;
 
+//        foreach(int i in scoreQuery){
+//         Console.WriteLine($"{i} ");
+        
+//        }
+//     }
+//     }
+// }
