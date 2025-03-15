f5aa9b2bd4b108ab33f35969eddc90d0604a94e0
Prajwol Shrestha <shresthaprajwol161@gmail.com>
2025-03-15 14:06:03 +0545

    wip: lamdaExpression

---
+++ lamdaExpression.cs
@@ -0,0 +1,17 @@
+using System;
+using System.Linq;
+using System.Collections.Generic;
+using System.Text;
+
+namespace lamdaExpressionExample{
+
+    class Program{
+    delegate int  Del(int i);
+    static void Main(string [] args){
+
+        Del d=y=>y*y;
+        int j=d(5);
+        Console.WriteLine(j);
+    }
+    }
+}
