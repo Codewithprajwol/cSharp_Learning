a13a0f56778a904343a2649a7a15a4b343ff4631
Prajwol Shrestha <shresthaprajwol161@gmail.com>
2025-03-15 13:43:49 +0545

    wip: generics in c#

--- Generics.cs
+++ Generics.cs
@@ -1,28 +1,28 @@
-using System;
-using System.Collections.Generic;
-using System.Collections;
-using System.Globalization;
+// using System;
+// using System.Collections.Generic;
+// using System.Collections;
+// using System.Globalization;
 
 
-namespace genericsExample{
-    class Program{
-        class Love<T,U>{
-            public required T name;
-            public required U age;
+// namespace genericsExample{
+//     class Program{
+//         class Love<T,U>{
+//             public required T name;
+//             public required U age;
 
-            public required T lover;
+//             public required T lover;
 
-            public void LovePlace<F>(F value){
-                    Console.WriteLine("love place is Sambriddi college and ture love is {0}",value);
-            }
+//             public void LovePlace<F>(F value){
+//                     Console.WriteLine("love place is Sambriddi college and ture love is {0}",value);
+//             }
 
-        }
-        static void Main(string [] args){
-            Love<string, int> mylove=new Love<string, int>(){name="parajwol",age=12,lover="parjwol"};
-            mylove.LovePlace<bool>(true);
-            // mylove.name="bhagwan";
-            // mylove.age=22;
-            // mylove.lover="prajwol";
-        }
-    }
-}
+//         }
+//         static void Main(string [] args){
+//             Love<string, int> mylove=new Love<string, int>(){name="parajwol",age=12,lover="parjwol"};
+//             mylove.LovePlace<bool>(true);
+//             // mylove.name="bhagwan";
+//             // mylove.age=22;
+//             // mylove.lover="prajwol";
+//         }
+//     }
+// }
