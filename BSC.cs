12984efe716ba6e5762a8143a8fc0a388fef16d9
Prajwol Shrestha <shresthaprajwol161@gmail.com>
2025-03-15 09:10:13 +0545

    wip:arrays in csharp

--- BSC.cs
+++ BSC.cs
@@ -9,7 +9,13 @@ namespace BasicLanguageConstruct{
 
             //   string [] fruits= new string [] {"apple", "banana", "orange"};
 
-              string [] fruits= ["apple", "banana", "orange"];
+              string [] fruits= ["apple", "banana", "orange"];//one dimensional array
+
+              int [,] numbers= new int [2,3]{// two dimensional array
+                {1,2,3},
+                {4,5,6}
+              };
+              Console.WriteLine(numbers[1,2]);// 
 
               foreach(string fruit in fruits){
                     Console.WriteLine(fruit);
@@ -20,6 +26,11 @@ namespace BasicLanguageConstruct{
             // fruits[1]="banana";
             // fruits[2]="orange";
             // Console.WriteLine($"The fruits are {fruits[0]}, {fruits[1]}, {fruits[2]}");
+
+            // jagged array
+            int [][] jaggedArray= new int [3][];
+            jaggedArray[0]= new int [2]{1,2};
+            jaggedArray[1]= new int [3]{3,4,5};
     }
 }
 }
