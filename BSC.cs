ccc281690dee5255d45c4312c5bbbe19d7736cdf
Prajwol Shrestha <shresthaprajwol161@gmail.com>
2025-03-15 09:24:25 +0545

    wip: basic of object and class in csharp

--- BSC.cs
+++ BSC.cs
@@ -1,36 +1,55 @@
-using System;
+// using System;
 
-namespace BasicLanguageConstruct{
+// namespace BasicLanguageConstruct{
 
-    class BSC{
-        static void Main(string[] args){
-            int x=10;
-            Console.WriteLine($"hello boy the number is {x}");
+//     class BSC{
+//         static void Main(string[] args){
+//             int x=10;
+//             Console.WriteLine($"hello boy the number is {x}");
 
-            //   string [] fruits= new string [] {"apple", "banana", "orange"};
+//             //   string [] fruits= new string [] {"apple", "banana", "orange"};
 
-              string [] fruits= ["apple", "banana", "orange"];//one dimensional array
+//               string [] fruits= ["apple", "banana", "orange"];//one dimensional array
 
-              int [,] numbers= new int [2,3]{// two dimensional array
-                {1,2,3},
-                {4,5,6}
-              };
-              Console.WriteLine(numbers[1,2]);// 
+//               int [,] numbers= new int [2,3]{// two dimensional array
+//                 {1,2,3},
+//                 {4,5,6}
+//               };
+//               Console.WriteLine(numbers[1,2]);// 
 
-              foreach(string fruit in fruits){
-                    Console.WriteLine(fruit);
-              }
+//               foreach(string fruit in fruits){
+//                     Console.WriteLine(fruit);
+//               }
 
-            // string [] fruits= new string [3];
-            // fruits[0]="apple";
-            // fruits[1]="banana";
-            // fruits[2]="orange";
-            // Console.WriteLine($"The fruits are {fruits[0]}, {fruits[1]}, {fruits[2]}");
+//             // string [] fruits= new string [3];
+//             // fruits[0]="apple";
+//             // fruits[1]="banana";
+//             // fruits[2]="orange";
+//             // Console.WriteLine($"The fruits are {fruits[0]}, {fruits[1]}, {fruits[2]}");
 
-            // jagged array
-            int [][] jaggedArray= new int [3][];
-            jaggedArray[0]= new int [2]{1,2};
-            jaggedArray[1]= new int [3]{3,4,5};
-    }
-}
-}
+//             // jagged array
+//             int [][] jaggedArray= new int [3][];
+//             jaggedArray[0]= new int [2]{1,2};
+//             jaggedArray[1]= new int [3]{3,4,5};
+
+
+//             foreach(int [] array in jaggedArray){
+//                 foreach(int number in array){
+//                     Console.WriteLine(number);
+//                 }
+//             }
+
+//             int z = 12;
+
+//             if (z == 12)
+//             {
+//                 goto customLabel;
+//             }
+            
+
+//             customLabel:
+//             Console.WriteLine("This is a custom label");// this is not working by the way
+
+//     }
+// }
+// }
---
+++ classAndobject.cs
@@ -0,0 +1,26 @@
+// using System;
+
+// namespace ClassAndObjectLearning{
+
+//     class User{
+//         public string name;
+//         public int age;
+//         // public User(string name, int age){
+//         //     this.name=name;
+//         //     this.age=age;
+//         // }
+//         public void Display(){
+//             Console.WriteLine($"name is {name} and age is {this.age}");
+//         }
+//     }
+
+
+//     class Program{
+//         static void Main(string[] args){
+//             var user1= new User();
+//             // var user1= new User{name="prajwol",age=21};//it is the shortcut for that of below
+//             user1.name="prajwol";
+//             user1.age=21;
+//             user1.Display();
+//     }
+// }}
